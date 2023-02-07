df9eab617ef03ed9e2eb072736dcc23dfa4a33a1
Aldokan <114017858+Aldokan@users.noreply.github.com>
2023-02-07 11:53:43 +0100

    Update control.c

--- control.c
+++ control.c
@@ -1,1 +1,13 @@
 #include "control.h"
+#include "elevio.h"
+
+void move(struct control c){
+    
+};
+
+void stop(struct control c){
+    if (c.status->bool_stop==1)
+    {
+       elevio_motorDirection(DIRN_STOP); 
+    };
+};
