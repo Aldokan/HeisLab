1b786ecf75c867c6368f89f109312d9be2585538
Aldokan <114017858+Aldokan@users.noreply.github.com>
2023-02-07 11:55:39 +0100

    Update status.h

--- status.h
+++ status.h
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <elevio.h>
 
 struct status
 {
     int bool_movement;
     int current_floor;
-    int floor_light;
+    int floor_light[4];
     int button_light[4];
     int bool_stop;
     int bool_obstruction;
